a937a8787b90f06502a8e01fd84e815768244e86
Rohan Brown <20042@jhc.school.nz>
2024-09-20 11:36:13 +1200

    Testing     added is_operation     added operations     added convert_string     added calculate
        Wanting to test convert_string & calculate

--- src/solving.py
+++ src/solving.py
@@ -1,20 +1,55 @@
+# -- OPERATIONS -- #    
+
+operations = {
+    "+": lambda numA, numB: numA + numB,
+    "-": lambda numA, numB: numA - numB,
+    "*": lambda numA, numB: numA * numB,
+    "/": lambda numA, numB: numA / numB
+}
+
 # -- FUNCTIONS -- #
 
-def convert_string(string):
+def is_operation(item):
+    '''
+    https://trello.com/c/49UxqLT7/30-is-operation
+    '''
+    return item in operations
+
+def convert_string(input_string):
     '''
     https://trello.com/c/z2vBybXd/25-convert-string
     '''
     list = []
 
-    for letter in string:
+    for letter in input_string:
+        letter = letter.strip()
         list.append(letter)
     
     try:
-        list.remove(" ")
+        while ' ' in list:
+            del list[' ']
+        while '' in list:
+            del list['']
     except:
-        return list
+       return list
     
+    for item in list:
+        item_index = list.index(item)
+        list[item_index] = int(item)
+
     return list
 
-def calculate():
-    return "as"
+def calculate(list):
+    full_index = len(list)
+    while 0 < int(full_index):
+        for item in list:
+            if is_operation(item):
+                item_index = list.index(item)
+                number_before = item_index - 1
+                number_after = item_index + 1
+                new_number = operations[item](number_before, number_after)
+                list[item_index] = new_number
+                del list[item_index - 1]
+                del list[item_index]  
+                print(list)   
+    return list
